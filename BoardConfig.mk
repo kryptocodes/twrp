2735bc9768564f25d831c8e4d1ab06aab11b9fff
iamsaikat <iamdevsaikat@gmail.com>
2018-04-01 01:02:49 +0530

    lettuce: Add support for d2tw

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,3 +79,6 @@ TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
 # Asian region languages
 TW_EXTRA_LANGUAGES := true
+
+# DT2W
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-0040/double_tap_enable"
